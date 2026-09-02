58012a2a6e4e6e38892298cd52c53ec00683ef48
walterl1 <fv8ybmn8m5@privaterelay.appleid.com>
2026-09-02 15:40:52 -0400

    loop until valid credentials

--- src/main/java/org/boif/Interface.java
+++ src/main/java/org/boif/Interface.java
@@ -7,10 +7,20 @@ public class Interface {
         System.out.println("Welcome to Bank of Insufficient Funds");
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter your account ID: ");
-            String accountId = scanner.nextLine();
-            System.out.print("Enter your PIN: ");
-            String pin = scanner.nextLine();
+            String accountId = "";
+            String pin = "";
+
+            while (true) {
+                System.out.print("Enter your account ID: ");
+                accountId = scanner.nextLine();
+                System.out.print("Enter your PIN: ");
+                pin = scanner.nextLine();
+
+                if (BankService.validate(accountId, pin))
+                    break;
+
+                System.out.println("Invalid credentials. Please try again.");
+            }
         }
     }
 }
